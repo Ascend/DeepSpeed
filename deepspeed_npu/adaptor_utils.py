3593e46795602b95e104db5be4efbbd072d76e79
jiaqiw <wangjiaqi50@huawei.com>
2023-09-25 20:50:51 +0800

    增加monkey_patch修正0.9.2版本deepspeed不支持模型保存问题

--- deepspeed_npu/adaptor_utils.py
+++ deepspeed_npu/adaptor_utils.py
@@ -3,7 +3,7 @@ import torch_npu
 
 from functools import wraps
 from deepspeed import comm as dist
-
+import DeepSpeed
 
 # recv/all_reduce operations need modify the inputs, copy back is required
 def wrapper_dist(fn):
@@ -38,7 +38,34 @@ def wrapper_dist_send(fn):
     return wrapper
 
 
+def wrapper_clone(fn):
+    @wraps(fn)
+    def clone_tensors_for_torch_save(*args, **kwargs):
+        """
+        Returns a copy of ``item`` with all enclosed tensors replaced by clones on a specified device.
+        Works on individual tensors, and tensors contained/nested in lists, tuples, and dicts.
+
+        Parameters:
+            - ``item``: tensor to clone or (possibly nested) container of tensors to clone.
+            - ``device``: target device (defaults to 'cpu')
+
+        Returns:
+            - copy of ``item`` with cloned tensors on target device
+        """
+        if torch.is_tensor(args[0]):
+            return args[0].detach().clone().to(args[1])
+        elif isinstance(args[0], list):
+            return [clone_tensors_for_torch_save(v, args[1]) for v in args[0]]
+        elif isinstance(args[0], tuple):
+            return tuple([clone_tensors_for_torch_save(v, args[1]) for v in args[0]])
+        elif isinstance(args[0], dict):
+            return type(args[0])({k: clone_tensors_for_torch_save(v, args[1]) for k, v in args[0].items()})
+        else:
+            return args[0]
+
 torch.cuda.nvtx = torch.ones
 dist.send = wrapper_dist_send(dist.send)
 dist.recv = wrapper_dist(dist.recv)
 dist.all_reduce = wrapper_dist(dist.all_reduce)
+DeepSpeed.deepspeed.checkpoint.utils.clone_tensors_for_torch_save = clone_tensors_for_torch_save(DeepSpeed.deepspeed.checkpoint.utils.clone_tensors_for_torch_save):
+
